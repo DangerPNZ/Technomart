f1d3aea5fe4982d95a78eac656e2a08fe85596a7
DangerPNZ <dangerpnz@mail.ru>
2016-10-17 02:03:33 +0400

    Обновление 3го поп-апа

--- js/script.js
+++ js/script.js
@@ -10,6 +10,13 @@ if (map_expand) {
         event.preventDefault();
         map_expand.classList.add("full_map_show");
     });
+    window.addEventListener("keydown", function(event) {
+        if (event.keyCode === 27) {
+            if (map_expand.classList.contains("full_map_show")) {
+                map_expand.classList.remove("full_map_show");
+            }
+        }
+    });
 }
 
 var feedback_link = document.querySelector(".link_mail");
@@ -38,28 +45,36 @@ if (feedback_open) {
             event.preventDefault();
         }
     });
+    window.addEventListener("keydown", function(event) {
+        if (event.keyCode === 27) {
+            if (feedback_open.classList.contains("feedback_show")) {
+                feedback_open.classList.remove("feedback_show");
+            }
+        }
+    });
 }
 
 var buy_link = document.querySelectorAll(".buy");
-
-window.addEventListener("keydown", function(event) {
-    if (event.keyCode === 27) {
-        if (map_expand.classList.contains("full_map_show")) {
-            map_expand.classList.remove("full_map_show");
+for (var i = 0; i < buy_link.length; i++) {
+    buy_link[i].addEventListener("click", function(event) {
+        event.preventDefault();
+        if (add_open) {
+            add_open.classList.add("add_product_show");
         }
-    }
-});
-
-window.addEventListener("keydown", function(event) {
-    if (event.keyCode === 27) {
-        if (feedback_open.classList.contains("feedback_show")) {
-            feedback_open.classList.remove("feedback_show");
+    });
+}
+var add_open = document.querySelector(".add_product");
+if (add_open) {
+    var add_close = add_open.querySelector(".btn_close");
+    add_close.addEventListener("click", function(event) {
+        event.preventDefault();
+        add_open.classList.remove("add_product_show");
+    });
+    window.addEventListener("keydown", function(event) {
+        if (event.keyCode === 27) {
+            if (add_open.classList.contains("add_product_show")) {
+                add_open.classList.remove("add_product_show");
+            }
         }
-    }
-});
-
-for(var i=0; i<buy_link.length; i++) {
-	buy_link[i].addEventListener("click", function(event) {
-  		event.preventDefault();
-	});
+    });
 }
